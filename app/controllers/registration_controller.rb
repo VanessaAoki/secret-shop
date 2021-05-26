a389fabfc529f041ca49e3b927fe3cdb0859fc15
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-05-26 02:27:25 -0300

    Style the update info

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,10 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def acount_update_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,11 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
   def show
+    @user = User.find(params[:id])
   end
 end
