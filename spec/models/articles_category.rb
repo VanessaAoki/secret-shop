a723c2088690b4359a7c1d9de39e92070d638605
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-05-26 00:13:21 -0300

    Fix articles category test

--- spec/models/articles_category.rb
+++ spec/models/articles_category.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ArticlesCategory, type: :model do
   describe 'Associations ' do
-    should belong_to(:article)
-    should belong_to(:category)
+    it { should belong_to(:article) }
+    it { should belong_to(:category) }
   end
 end
