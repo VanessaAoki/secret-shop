c7f4548f217ac616e65bb0e6a48d7bfdde3356c6
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-05-26 09:52:24 -0300

    Fix categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,28 @@
 class CategoriesController < ApplicationController
+  # GET /categories or /categories.json
   def index
-    @category = Category.all
+    @categories = Category.includes(:articles).all
+    articles = Article.all
+    
+    @articles_by_category = []
+    @categories.each do |category|
+      @articles_by_category << recent_article(category) unless recent_article(category).nil?
+    end
   end
 
+  # GET /categories/1 or /categories/1.json
   def show
     @category = Category.includes(:articles).find(params[:id])
     recent_related_articles
+  end
+
+  private
+
+  def recent_related_articles
+    @recent_related_articles ||= @category.articles.ordered_by_most_recent.limit(4)
+  end
+
+  def recent_article(category)
+    category.articles.ordered_by_most_recent.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registration: 'registrations' }
  
   resources :users, only: [:index, :show]
+  resources :categories, only: [:index, :show]
   resources :articles, only: %i[new create destroy show index edit] do 
     resources :votes, only: %i[create destroy]
   end
