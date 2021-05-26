d16ff8110206a928d2afead126f7b6017664f2c2
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-05-26 01:05:26 -0300

    Style the root page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+  def user_navbar
+    out = ''
+    if user_signed_in?
+      out += '<p>'
+      out += link_to current_user.name, edit_user_registration_path, class: 'button ml-2 is-navbar'
+      out += '</p>'
+      out += '<p>'
+      out += link_to 'Logout', destroy_user_session_path, method: :delete, class: 'button ml-2 is-navbar'
+    else
+      out += '</p>'
+      out += '<p>'
+      out += link_to 'Log In', new_user_session_path, class: 'button is-navbar'
+      out += '</p>'
+      out += '<p>'
+      out += link_to 'Sign Up', new_user_registration_path, class: 'button is-navbar'
+      out += '</p>'
+    end
+    out.html_safe
+  end
+  
+  def render_sign_in
+    out = ''
+    if current_user
+      out << link_to(current_user.name, user_path(id: current_user.id))
+      out << link_to('Sign out', destroy_user_session_path, method: :delete)
+    else
+      out << link_to('Sign in', user_session_path)
+    end
+    out.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
-Rails.application.routes.draw do
-  get 'vote/create'
-  get 'vote/destroy'
-  get 'user/show'
-  devise_for :users,  :controllers  => { registrations: 'registrations' }
-
-  resources :articles
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+Rails.application.routes.draw do  
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  
+  resources :articles, only: %i[new create destroy show index edit] do 
+    resources :votes, only: %i[create destroy]
+  end
+  
+  root 'articles#index'
 end
