36732acf103c4266e88ea57205c4d2bfffc4e93b
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-06-01 18:21:19 -0300

    Fix add article

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,16 +4,16 @@ module ArticlesHelper
       link_to 'New Article', new_article_path
     end
   end
-
-  def articles_controller
-    out = ''
-    @articles.each do |article|
-      out += "<tr>"
-      out += "<td>#{link_to 'Show', article}</td>"
-      out += "<td>#{link_to 'Edit', edit_article_path(article)}</td>"
-      out += "<td>#{link_to 'Destroy', article, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
-      out += "</tr>"
+  
+  def articles_controller(article)
+    out = '<div>'
+    if current_user.admin == true
+      out += link_to 'Edit', edit_article_path(@article), class: 'button is-warning is-outlined mr-2'
+      out += button_to 'Delete', article, method: :delete, data: { confirm: 'Are you sure?' },
+                                        class: 'button is-danger is-outlined mr-2'
     end
+    out += link_to 'Home', articles_path, class: 'button is-primary is-outlined'
+    out += '</div>'
     out.html_safe
   end
 end
