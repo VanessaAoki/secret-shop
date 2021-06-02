423e4349728a60e13db03cd4bf05bbe6e79488b1
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-06-02 01:20:20 -0300

    Style the news articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -10,5 +10,22 @@ module ArticlesHelper
     out += '</div>'
     out.html_safe
   end
+
+  def show_new_article_list(articles)
+    out = ''
+      articles.each do |article|
+        out += "<div class=\"column p-0 article-categories is-3\">"
+        out += "<div class=\"dummy\"></div>"
+        out += image_tag (article.image), class: "article-categories-image"
+        out += "</div>"
+        out += "<div class=\"column p-5 article-news is-3 has-background-white has-text-main has-text-weight-bold\">"
+        out += "<h1 class=\"has-text-accent\">News</h1>"
+        out += "<h2 class=\"my-5 has-text-weight-semibold\">#{article.title}</h2>"
+        out += "<p class=\"mb-5 has-text-weight-normal articles-news-main\">#{article.text}</p>"
+        out += "<span class=\"mb-5 has-text-accent is-size-09 has-text-weight-normal\">#{link_to 'Read More', article_url(article)}</span>"
+        out += '</div>'
+      end
+      out.html_safe
+  end
 end
 
