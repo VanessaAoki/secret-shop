11eb059cb1e0c2b61bdb4bd3650cfc3ab8553dfa
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-06-02 22:37:57 -0300

    Add final styling to articles show

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,11 +2,10 @@ module ArticlesHelper
   def articles_controller(article)
     out = "<div class=\"is-flex\">"
     if current_user.admin == true
-      out += link_to 'Edit', edit_article_path(@article), class: 'button is-warning is-outlined mr-2'
+      out += link_to 'Edit', edit_article_path(@article), class: 'button is-warning mr-2'
       out += button_to 'Delete', article, method: :delete, data: { confirm: 'Are you sure?' },
-                                          class: 'button is-danger is-outlined mr-2'
+                                          class: 'button is-danger mr-2'
     end
-    out += link_to 'Home', articles_path, class: 'button is-primary is-outlined'
     out += '</div>'
     out.html_safe
   end
@@ -28,28 +27,5 @@ module ArticlesHelper
       out += '</div>'
     end
     out.html_safe
-  end
-
-  def link(articles, index)
-    link_to(
-      content_tag(:div, nil, class: 'hero-head ml-5 mt-4') do
-        content_tag(:p, @categories.find(index + 1).name,
-                    class: 'title has-text-white') +
-          content_tag(:p, nil, class: 'hero-body pt-6 has-text-white') +
-          (content_tag(:p, articles.title, class: 'hero-foot subtitle size-6 mb-2') if articles)
-      end,
-      article_path(articles), class: 'has-text-black column',
-                              style: "background: url('#{display_photo(articles)}') center center; background-size: cover"
-    )
-  end
-  
-  def show_articles_by_categories(articles)
-    return if articles.nil?
-
-    content_tag(:div, nil, class: 'columns is-gapless') do
-      articles.each_with_index do |articles, index|
-        concat(link(articles, index))
-      end
-    end
   end
 end
