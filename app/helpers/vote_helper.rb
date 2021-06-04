385210ec952663c8f4eb40e364802e4e8ba845b3
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-06-04 01:50:38 -0300

    Fix vote button

--- app/helpers/vote_helper.rb
+++ app/helpers/vote_helper.rb
@@ -3,9 +3,9 @@ module VoteHelper
     if current_user
       vote = Vote.find_by(article: art, user: current_user)
       if vote
-        link_to('Click to Unvote!', article_vote_path(id: vote.id, article_id: art.id), method: :delete)
+        button_to('Click to Unvote!', article_vote_path(id: vote.id, article_id: art.id), method: :delete, class: 'button is-accent')
       else
-        link_to('Click to Vote!', article_votes_path(article_id: art.id), method: :post)
+        button_to('Click to Vote!', article_votes_path(article_id: art.id), method: :post, class: 'button is-accent')
       end
     else
       link_to('LOG IN to vote!', user_session_path)
