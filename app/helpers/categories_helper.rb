2080adeadf7e969c2e0915bfdda20fee3461d163
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-06-03 23:36:15 -0300

    Fix styling the categories show

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,16 +1,19 @@
 module CategoriesHelper
   def show_article(category)
-    out = "<div class=\"columns is-multiline\">"
+    out = "<div class=\"columns is-multiline m-0\">"
     category.articles.each do |article|
       article.categories.each do |category|
-        out += "<div class=\"column is-4 image-container\">"
+        out += "<article class=\"columns column is-12 categories-show p-0 m-0\">"
+        out += "<div class=\"column is-5 image-container p-0\">"
         out += image_tag article.image, class: 'categories-show-image' if article.image.attached?
         out += '</div>'
-        out += '<div class="column is-8 p-5 article-news has-background-white has-text-main has-text-weight-bold">'
-        out += "<h2 class=\"my-5 has-text-weight-semibold\">#{article.title}</h2>"
-        out += "<p class=\"mb-5 has-text-weight-normal articles-news-main\">#{article.text}</p>"
-        out += "<span class=\"mb-5 has-text-accent is-size-09 has-text-weight-normal\">#{link_to 'Read More', article_url(article)}"
+        out += '<div class="column is-7 p-4 article-news has-background-white has-text-main is-flex is-flex-direction-column is-justify-content-space-around">'
+        out += "<h2 class=\"has-text-weight-semibold\">#{article.title}</h2>"
+        out += "<p class=\"has-text-weight-normal articles-news-main\">#{article.text}</p>"
+        out += "<span class=\"has-text-accent is-size-09 has-text-weight-normal\">#{link_to 'Read More', article_url(article)}"
         out += '</span>'
+        out += '</div>'
+        out += '</article>'
         out += '</div>'
       end
     end
