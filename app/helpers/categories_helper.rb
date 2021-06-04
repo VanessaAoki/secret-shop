0ee785211fed52ccdd9ed15ba05c2d3b31633aa0
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-06-03 23:51:25 -0300

    Fix category show styling

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -3,11 +3,11 @@ module CategoriesHelper
     out = "<div class=\"columns is-multiline m-0\">"
     category.articles.each do |article|
       article.categories.each do |category|
-        out += "<article class=\"columns column is-12 categories-show p-0 m-0\">"
-        out += "<div class=\"column is-5 image-container p-0\">"
+        out += "<article class=\"columns column is-12 categories-show p-0 m-0 mb-4\">"
+        out += "<div class=\"column is-4 image-container p-0\">"
         out += image_tag article.image, class: 'categories-show-image' if article.image.attached?
         out += '</div>'
-        out += '<div class="column is-7 p-4 article-news has-background-white has-text-main is-flex is-flex-direction-column is-justify-content-space-around">'
+        out += '<div class="column is-8 p-4 article-news has-background-white has-text-main is-flex is-flex-direction-column is-justify-content-space-around">'
         out += "<h2 class=\"has-text-weight-semibold\">#{article.title}</h2>"
         out += "<p class=\"has-text-weight-normal articles-news-main\">#{article.text}</p>"
         out += "<span class=\"has-text-accent is-size-09 has-text-weight-normal\">#{link_to 'Read More', article_url(article)}"
