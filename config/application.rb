911a67fef8ce7068a367766fb696aa5e8e8fa967
Vanessa Aoki <vanessa.aoki@outlook.com>
2021-06-06 22:39:57 -0300

    Add autoloader classic

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module SecretShop
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoloader = :classic
   end
 end
